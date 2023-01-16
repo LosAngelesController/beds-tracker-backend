d135807c31565d18064e15cd6d57c3a5d56b7ff4
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-16 15:34:00 -0800

    sdfjkhsdfhkjfsdhkjsfdhkjfsdkfs

--- src/postgres.ts
+++ src/postgres.ts
@@ -1,18 +1,30 @@
 import fs from 'fs'
 
+var argv = require('optimist').argv;
+
 const config = require('../config.json');
 
-const { Client } = require('pg')
-export const pgclient = new Client({
-  user: config.db.username,
-  host: config.db.host,
-  database: config.db.database,
-  password: config.db.password,
-  port: config.db.port,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('./server-ca.pem').toString(),
-    key: fs.readFileSync('./client-key.pem').toString(),
-    cert:  fs.readFileSync('./client-cert.pem').toString(),
+  var pgclientconfig = {
+    user: config.db.username,
+    host: config.db.host,
+    database: config.db.database,
+    password: config.db.password,
+    port: config.db.port,
+    
   }
-})
+
+if (argv.nossl) {
+
+} else {
+  pgclientconfig['ssl'] = {
+      rejectUnauthorized: false,
+      ca: fs.readFileSync('./server-ca.pem').toString(),
+      key: fs.readFileSync('./client-key.pem').toString(),
+      cert:  fs.readFileSync('./client-cert.pem').toString()
+
+      }    }
+
+
+
+const { Client } = require('pg')
+export const pgclient = new Client(pgclientconfig)
