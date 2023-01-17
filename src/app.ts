c94d693b9eb46da5eabf402f2ec76aecb3305e32
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-16 19:29:48 -0800

    backend new service

--- src/app.ts
+++ src/app.ts
@@ -142,10 +142,10 @@ async function main() {
       
       var vendorparams = [args.querystring];
 
-      if (aliasforwarding[args.querystring]) {
+      if (aliasforwarding[args.querystring.toUpperCase()]) {
         vendorquery = "SELECT * FROM vendors_summed WHERE (vendor_name ILIKE '%' || $1 || '%') OR (vendor_name ILIKE '%' || $2 || '%') ORDER BY sum desc LIMIT 100;"
       
-        vendorparams = [args.querystring, aliasforwarding[args.querystring]];
+        vendorparams = [args.querystring, aliasforwarding[args.querystring.toUpperCase()]]];
       }
 
       if (typeof args.querystring === "string") {
