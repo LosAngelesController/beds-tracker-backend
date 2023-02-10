376e1baf201f79e9d497f7413fce4cd45e8ba1e0
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-02-10 08:35:31 -0800

    Revert "Revert "backend tracker""
    This reverts commit c465147c91eb3063840e9e03e66fc99804de759c.

---
+++ src/daemon.ts
@@ -0,0 +1,41 @@
+import {pgclient} from './postgres'
+import cors from 'cors'
+
+var argv = require('optimist').argv;
+
+
+//DJKenster is the http api endpoint for checkbook.
+//https://djkenster.checkbook.mejiaforcontroller.com/vendorpage/
+
+const express = require('express')
+const app = express()
+const port = argv.port || 3713;
+
+async function daemon() {
+    await pgclient.connect()
+    const restest = await pgclient.query('SELECT * FROM shelters', [])
+    console.log(restest.rows)
+    // Hello world!
+
+ 
+
+app.get('/', (req, res) => {
+  res.type('html');
+  res.send('Hello World!');
+});
+
+app.get('/shelters', cors(), async (req, res) => {
+  const restest = await pgclient.query('SELECT * FROM shelters', [])
+  res.json({
+    rows: restest.rows
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+  
+    
+  }
+  
+daemon()
